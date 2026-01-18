397e8b15f7a9011e773313bb3171c7aa13345cb0
dominiktopchick <149238434+dominiktopchick@users.noreply.github.com>
2026-01-18 13:50:10 +0200

    Update settings.py

--- gmaeeee/settings.py
+++ gmaeeee/settings.py
@@ -3,22 +3,21 @@ import pygame
 WIDTH, HEIGHT = 1000, 700
 FPS = 60
 
+# --- ШЛЯХИ ДО КАРТИНОК (змінюй тут) ---
+IMG_PLAYER = "player.png"
+IMG_ENEMY_BASIC = "enemy_basic.png"
+IMG_ENEMY_ORANGE = "enemy_orange.png"
+IMG_ENEMY_SNIPER = "enemy_sniper.png"
+IMG_MEDKIT = "medkit.png"
+
 # Кольори
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-HEALTH_CLR = (255, 50, 50)
-AMMO_CLR = (255, 200, 50)
-DARK_GREEN = (15, 40, 15)
-TREE_COLOR = (10, 50, 10)
-ROCK_COLOR = (100, 100, 100)
-GRASS_COLOR = (40, 100, 40)
+WHITE, BLACK = (255, 255, 255), (0, 0, 0)
+HEALTH_CLR, AMMO_CLR = (255, 50, 50), (255, 200, 50)
+DARK_GREEN, GRASS_COLOR = (15, 40, 15), (40, 100, 40)
+TREE_COLOR, ROCK_COLOR = (10, 50, 10), (100, 100, 100)
 PLAYER_CLR = (0, 100, 255)
-ENEMY_BASIC = (200, 0, 0)
-ENEMY_ORANGE = (255, 165, 0)
-ENEMY_SNIPER = (128, 0, 128)
-MEDKIT_CLR = (255, 50, 50)
-CROSSHAIR_CLR = (0, 255, 0) # Зелений приціл
+ENEMY_BASIC, ENEMY_ORANGE, ENEMY_SNIPER = (200, 0, 0), (255, 165, 0), (128, 0, 128)
+CROSSHAIR_CLR = (0, 255, 0)
 
 # Баланс
-START_COINS = 0
-PLAYER_HP = 100
+START_COINS, PLAYER_HP = 100, 100
